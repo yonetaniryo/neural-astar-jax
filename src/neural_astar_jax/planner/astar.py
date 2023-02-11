523d26407d57f6622106ae63645ee6edbfe253d8
Ryo Yonetani <yonetani.vision@gmail.com>
2023-02-12 01:46:02 +0900

    vanilla astar

--- src/neural_astar_jax/planner/astar.py
+++ src/neural_astar_jax/planner/astar.py
@@ -11,29 +11,42 @@ class CNN(nn.Module):
     @nn.compact
     def __call__(self, map_designs, start_maps, goal_maps):
 
-        x = jnp.concatenate((map_designs, start_maps + goal_maps), -1)
+        x = jnp.stack((map_designs, start_maps + goal_maps), -1)
 
         for i, b in enumerate([32, 64, 128, 256, 1]):
             x = nn.Conv(features=b, kernel_size=(3, 3))(x)
             x = nn.LayerNorm()(x)
             x = nn.relu(x) if i < 3 else nn.sigmoid(x)
 
-        return x
+        return x[..., 0]
 
 
-class NeuralAstar(nn.Module):
+class VanillaAstar(nn.Module):
+    def setup(self):
+        astar = DifferentiableAstar()
+        self.astar = jax.vmap(astar)
+
+    def encode(self, map_designs, start_maps, goal_maps):
+        return map_designs
+
+    def __call__(self, map_designs, start_maps, goal_maps):
+        cost_maps = self.encode(map_designs, start_maps, goal_maps)
+        astar_output = self.astar(
+            cost_maps,
+            start_maps,
+            goal_maps,
+            map_designs,
+        )
+
+        return astar_output
+
+
+class NeuralAstar(VanillaAstar):
     def setup(self):
         astar = DifferentiableAstar()
         self.astar = jax.vmap(astar)
         self.encoder = CNN()
 
-    def __call__(self, map_designs, start_maps, goal_maps):
+    def encode(self, map_designs, start_maps, goal_maps):
         cost_maps = self.encoder(map_designs, start_maps, goal_maps)
-        astar_output = self.astar(
-            cost_maps[..., 0],
-            start_maps[..., 0],
-            goal_maps[..., 0],
-            map_designs[..., 0],
-        )
-
-        return astar_output
+        return cost_maps
